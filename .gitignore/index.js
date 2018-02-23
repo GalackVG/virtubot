6e71c218184545103345bd8beec24512428eeacb
GalackVG <36767921+GalackVG@users.noreply.github.com>
2018-02-23 20:43:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -46,7 +46,69 @@ bot.on('message', message =>{
     }
         if (message.content === "*créateur"){
             message.reply("Mon créateur et Galack. :)");
-            console.log("Commande Salut effectué");const Discord = require('discord.js');
+            console.log("Commande Salut effectué");const Discord = require('discord.js');const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: *help");
+    console.log("Connecter");
+});
+
+bot.login ("NDE2NjUzMTgwNDY2NzU3NjM0.DXHmrQ.Ab4bNavUkCfyuXT3eTG1c1rRlUE");
+
+
+bot.on('message', message =>{
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n - *help \n - *créateur \n - *créationbot");
+    }
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+    }
+    if (message.content === "salut"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+
+    }
+    if (message.content === "yo"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+
+    }
+    if (message.content === "Bonjour"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+
+    }
+    if (message.content === "bonjour"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+
+    }
+    if (message.content === "*créationbot"){
+        message.reply("Création du bot le 23/02/2018 à 20h13 ");
+        console.log("Commande Salut effectué");
+
+    }
+    if (message.content === "*créateur"){
+            message.reply("Mon créateur et Galack. :)");
+            console.log("Commande Salut effectué");
+
+    }
+    if (message.content === prefix + "embed"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("TEST EMBED")
+            .description("Ceci et un test embed")
+            .addField(".help","Page d'aide", true)
+            .addField("Embed01","Embed 01 ! :) Rejoins-nous sur notre forum (https://virtuality-ghost.eu", true)
+            .setColor("0x2E64FE")
+            .setFooter("Passe un bon moment parmis nous! :D")
+        message.channel.sendEmbed(embed);
+    }
+   
+});
 const bot = new Discord.Client();
 
 var prefix = ("*")
